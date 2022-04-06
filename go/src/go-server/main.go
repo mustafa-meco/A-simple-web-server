db3684373e567828d8d1ff20e5ad95a94c213a86
Mustafa <musgamal05@gmail.com>
2022-04-06 13:47:46 +0200

    test

--- go/src/go-server/main.go
+++ go/src/go-server/main.go
@@ -6,8 +6,30 @@ import (
 	"net/http"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w,"ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name: %s\n", name)
+	fmt.Fprintf(w, "Address: %s\n", address)
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", )
+		return
+	}
 	
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusMethodNotFound)
+		return
+	}
+	
+	fmt.Fprintf(w, "hello!")
 }
 
 func main(){
